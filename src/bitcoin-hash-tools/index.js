029a2b1129ce1515ea16d46ad011ff01f73fe6fc
The Color Blocks <thecolorblocks@proton.me>
2023-05-12 22:29:52 +0800

    Update again

--- src/bitcoin-hash-tools/index.js
+++ src/bitcoin-hash-tools/index.js
@@ -3,6 +3,7 @@ import jsSHA from 'jssha'
 function computeTxId(txHex) {
   const hashFn1 = new jsSHA("SHA-256", "HEX")
   const firstHash = hashFn1.update(txHex).getHash('HEX')
+  console.log(firstHash)
   const hashFn2 = new jsSHA("SHA-256", "HEX")
   const secondHash = hashFn2.update(firstHash).getHash('HEX')
   return reverseBytes(secondHash)
@@ -16,17 +17,8 @@ function computeBlockHash(blockHeaderHex) {
   return reverseBytes(secondHash)
 }
 
-function hexToBytes(hex) {
-  for (var bytes = [], c = 0; c < hex.length; c += 2)
-      bytes.push(parseInt(hex.substr(c, 2), 16));
-  return new Uint8Array(bytes);
-}
-
 function reverseBytes(hex) {
-  var bytes = [];
-  for(var i=0; i< hex.length-1; i+=2)
-      bytes.unshift(hex.substr(i, 2));
-  return bytes.join('');
+  return hex.match(/.{2}/g).reverse().join('');
 }
 
 export { computeTxId, computeBlockHash }
